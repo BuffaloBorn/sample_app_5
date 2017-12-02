1923a321e1b2d2e3d2e31886d8275de5e4bfd8a7
carl-mcgruder <carl.mcgruder@gmail.com>
2017-12-02 09:03:43 -0500

    Exercises - Change route to use signup_path instead of users_new_url.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'#, as: 'helf'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get  'users/new'
+  get  '/signup',  to: 'users/new'
 end
