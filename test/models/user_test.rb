24e70c20baaabed81caf37d87dce95a996fca410
carl-mcgruder <carl.mcgruder@gmail.com>
2017-12-02 22:07:32 -0500

    A test for validation of the email attribute. red

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,4 +15,8 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
 end
