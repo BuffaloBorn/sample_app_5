cf3107ec757801bbf5830aa400b35266a0874bd5
carl-mcgruder <carl.mcgruder@gmail.com>
2017-12-06 17:30:30 -0500

    Ex - In app/controllers/users_controller.rb, changed @user.save to false
    Failure:
    UsersSignupTest#test_valid_signup_information
    [C:/Users/camcgruder/git/sample_app_5/test/integration/
    users_signup_test.rb:19]:
    "User.count" didn't change by 1.
    Expected: 1
      Actual: 0
    
    bin/rails test test/integration/users_signup_test.rb:17

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if false
       flash[:success] = "Welcome to the Sample App!"
-      #redirect_to @user
+      redirect_to @user
     else
       render 'new'
     end
