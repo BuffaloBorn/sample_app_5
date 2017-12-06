03d72930f53fc656a54197de996d6ec34d440ca0
carl-mcgruder <carl.mcgruder@gmail.com>
2017-12-06 16:55:53 -0500

    Write a test for the flash implemented in previous commit. How detailed you want to make your tests is up to you; a suggested ultra-minimalist template appears in this commit, which you should complete by replacing FILL_IN with the appropriate code. (Even testing for the right key, much less the text, is likely to be brittle, so I prefer to test only that the flash isn’t empty.)
    To assist in figure out the right syntax for this situation.
    
    Here I used, flash.now.notice = "Good luck now!" to replace FILL_IN
    
    api.rubyonrails.org/classes/ActionDispatch/Flash/FlashHash.html#method-i-now

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,6 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.FILL_IN
+    assert flash.now.notice = "Welcome to the Sample App!"
   end
 end
