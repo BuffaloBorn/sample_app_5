605d55f458b415e8429d301170c3e35f8d0a2310
carl-mcgruder <carl.mcgruder@gmail.com>
2017-12-08 00:08:46 -0500

    A test of login after signup. green
    we can assert that the user is logged in after signup with commit

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -25,5 +25,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert flash.now.notice = "Welcome to the Sample App!"
+    assert is_logged_in?
   end
 end
