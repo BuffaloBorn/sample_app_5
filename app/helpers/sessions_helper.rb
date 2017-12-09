b24a050d92d536cb4dd20ccadd79a498e94e1b05
carl-mcgruder <carl.mcgruder@gmail.com>
2017-12-08 22:35:38 -0500

    Remembering the user.
    As with log_in, commit: Logging in and remembering a user, defers the
    real work to the Sessions helper, where we define a remember method
    that calls user.remember, thereby generating a remember token and saving
    its digest to the database. It then uses cookies to create permanent
    cookies for the user id and remember token as described above.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # Returns the current logged-in user (if any).
   def current_user
    @current_user ||= User.find_by(id: session[:user_id])
