036efbb1ef1f7cd6a47afa35ab777f252487c1a9
carl-mcgruder <carl.mcgruder@gmail.com>
2017-12-09 19:38:30 -0500

    Logging out from a persistent session. green
    With the code in commit: Adding a forget method to the User model,
    we’re now ready to forget a permanent session by adding a forget helper
    and calling it from the log_out helper in this commit. As seen iin this
    commit, the forget helper calls user.forget and then deletes the
    user_id and remember_token cookies.
    
    At this point, the tests suite should be green:
    
    $ rails test
    
    After testing, I've discovered that VALID_EMAIL_REGEX was not
    constructed properly so it failed validates process.
    
    Exercises -
    
    1. After logging out, verify that the corresponding cookies have been
    removed from your browser.
    
    Yes, I see that user_id and remember_token cookie has been created on
    login and both as been deleted on logout.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,18 @@
-pp/models/user.rb
- class User < ApplicationRecord
+class User < ApplicationRecord
   attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                      format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+                                                BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
@@ -32,7 +31,7 @@ pp/models/user.rb
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
-
+  
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
