43f18691264b677c6d2ed0ab515f67f774af359a
carl-mcgruder <carl.mcgruder@gmail.com>
2017-12-14 23:44:42 -0500

    Testing that edit and update are protected. red
    Because the before filter operates on a per-action basis, we’ll put the
    corresponding tests in the Users controller test. The plan is to hit the
    edit and update actions with the right kinds of requests and verify that
    the flash is set and that the user is redirected to the login path. From
    a table below, we see that the proper requests are GET and PATCH,
    respectively, which means using the get and patch methods inside the
    tests.
    
    Note that the second test shown in this commit involves using the patch
    method to send a PATCH request to user_path(@user). According to table
    below:
    
    ```
    HTTP
    request    URL          Action    Named route           Purpose
    GET        /users              index      users_path            * page
    to list all
                                                            users
    GET        /users/1          show         user_path(user)         * page
    to show user
    GET        /users/new      new      new_user_path           * page to
    make a
                                                            new user
    (signup)
    POST       /users              create   users_path              * create
    a new user
    GET        / users/1/edit  edit           edit_user_path(user) page to
    edit
                                                           user with id 1
    PATCH    /users/1            update   user_path(user)     * update user
    DELETE   /users/1            destroy  user_path(user)     * delete user
    ```
    
    such a request gets routed to the update action in the Users controller.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,5 +6,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 end
