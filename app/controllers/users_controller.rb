54db576522cac11f635fb6172c5c64b57a08f4a9
carl-mcgruder <carl.mcgruder@gmail.com>
2017-12-15 23:35:33 -0500

    The user index action.
    To display the users themselves, we need to make a variable containing
    all the site’s users and then render each one by iterating through them
    in the index view. As you may recall from the corresponding action in
    the toy app, we can use User.all to pull all the users out of the
    database, assigning them to an @users instance variable for use in the
    view, as seen in Listing 10.36. (If displaying all the users at once
    seems like a bad idea, you’re right, and we’ll remove this blemish)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,9 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
 
   def index
+     @users = User.all
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
