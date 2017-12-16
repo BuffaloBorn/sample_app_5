d43cb7c7d55192adcc8ae5d78c1d15d590e81ca6
carl-mcgruder <carl.mcgruder@gmail.com>
2017-12-15 21:00:29 -0500

    The Sessions create action with friendly forwarding. green
    To implement the forwarding itself, we use the redirect_back_or method
    to redirect to the requested URL if it exists, or some default URL
    otherwise, which we add to the Sessions controller create action to
    redirect after successful login (this commit). The redirect_back_or
    method uses the or operator || through
    
    session[:forwarding_url] || default
    
    This evaluates to session[:forwarding_url] unless it’s nil, in which
    case it evaluates to the given default URL. Note that commit: Code to
    implement friendly forwarding is careful to remove the forwarding URL
    (via session.delete(:forwarding_url)); otherwise, subsequent login
    attempts would forward to the protected page until the user closed their
    browser. (Testing for this is left as an exercise. Also note that the
    session deletion occurs even though the line with the redirect appears
    first; redirects don’t happen until an explicit return or the end of the
    method, so any code appearing after the redirect is still executed.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      params[:session][:remember_me] == '1' ? remember(@user) :
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
