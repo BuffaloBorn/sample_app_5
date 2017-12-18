c63368615f76127d38a595f709d490a50359590e
carl-mcgruder <carl.mcgruder@gmail.com>
2017-12-17 19:03:12 -0500

    Testing that the admin attribute is forbidden.
    Exercises -
    
    By issuing a PATCH request directly to the user path as shown in this
    commit, verify that the admin attribute isn’t editable through the web.
    To be sure your test is covering the right thing, your first step should
    be to add admin to the list of permitted parameters in user_params so
    that the initial test is red.
    
    We to make sure that this is red by running:
    
    $ rails test
    
    ERROR["test_should_not_allow_the_admin_attribute_to_be_edited_via_the_web",
    UsersControllerTest, 19.400911097007338]
    test_should_not_allow_the_admin_attribute_to_be_edited_via_the_web#UsersControllerTest
    (19.40s)
    NameError:         NameError: uninitialized constant
    UsersControllerTest::FILL_IN
                test/controllers/users_controller_test.rb:52:in `block in
    <class:UsersControllerTest>'
    
    =E

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
 
    def user_params
      params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
+                                  :password_confirmation, :admin)
    end
 
    # Before filters
