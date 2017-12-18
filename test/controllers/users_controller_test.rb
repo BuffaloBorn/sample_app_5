b4568a5261f65a6a4164cf74edc9179c7eefac08
carl-mcgruder <carl.mcgruder@gmail.com>
2017-12-17 22:00:30 -0500

    Exercises - should not allow the admin attribute to be edited via the web
    This question wasn't that straight forward to me when I first read it.
    
    I was thinking that that we needed to pass in the password and
    password_confirmation from wthin the object reference from @other_user
    instance variable but in case we are concern about any attribute that
    isn’t editable and writing such a test for the admin attribute
    
    You must reload the instance variable after making changes to the
    underlying record. This will load in the new changes but we must make
    sure that that password relate params are set to empty string.
    
    [should not allow the admin attribute to be edited via the web]
    (https://stackoverflow.com/questions/26270102/railstutorial-org-michael-hartl-chapter-9-exercise-3-should-not-allow-the-admin/26270476)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -49,9 +49,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     log_in_as(@other_user)
     assert_not @other_user.admin?
     patch user_path(@other_user), params: {
-                                   user: { password:              FILL_IN,
-                                           password_confirmation: FILL_IN,
-                                           admin: FILL_IN } }
-    assert_not @other_user.FILL_IN.admin?
+                                   user: { password: '',
+                                           password_confirmation: '',
+                                           admin: true } }
+    assert_not @other_user.reload.admin?
   end
 end
