b9d05c5176fc205508d54539ae5c397f58c8512b
carl-mcgruder <carl.mcgruder@gmail.com>
2017-12-28 12:16:42 -0500

    Setting the test domain host.
    With the code as above, the mailer test should be green:
    
    s```
    $ rails test:mailers
    ```
    
    Exercises -
    
    1. Verify that the full test suite is still green.
    
    Yes, everything is green after adding host parameter.
    
    2. Confirm that the test goes red if you remove the call to CGI.escape
    in commit: A test of the current email implementation.
    
    After remove the call to CGI.escape, the test mailer did not got RED but
    stayed GREEN. Below is the body of the email:
    
    ----==_mimepart_5a452624ce111_39e1c139e76064441
    Content-Type: text/plain;
     charset=UTF-8
    Content-Transfer-Encoding: 7bit
    
    Hi Michael Example,
    
    Welcome to the Sample App! Click on the link below to activate your
    account:
    
    http://localhost:3000/account_activations/b5Hne35KnLfmfTNgAiek4g/edit?email=michael%40example.com
    
    ----==_mimepart_5a452624ce111_39e1c139e76064441
    Content-Type: text/html;
     charset=UTF-8
    Content-Transfer-Encoding: 7bit
    
    <!DOCTYPE html>
    <html>
      <head>
        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"
    />
        <style>
          /* Email styles need to be inline */
        </style>
      </head>
    
      <body>
        <h1>Sample App</h1>
    
    <p>Hi Michael Example,</p>
    
    <p>
    Welcome to the Sample App! Click on the link below to activate your
    account:
    </p>
    
    <a
    href="http://localhost:3000/account_activations/b5Hne35KnLfmfTNgAiek4g/edit?email=michael%40example.com">Activate</a>
    
      </body>
    </html>
    
    ----==_mimepart_5a452624ce111_39e1c139e76064441--
     And it does contain the encoded email regardless of having the
    assertion there of not.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,23 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'                     # Local server
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
+  # Change mail delvery to either :smtp, :sendmail, :file, :test
+config.action_mailer.delivery_method = :smtp
+#config.action_mailer.delivery_method = :letter_opener
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "cem.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
 
   config.action_mailer.perform_caching = false
 
@@ -51,8 +67,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'                     # Local server
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
 end
