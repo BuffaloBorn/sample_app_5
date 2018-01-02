1938dbe041f351e0cc0069585e49925fb6eac2e8
carl-mcgruder <carl.mcgruder@gmail.com>
2018-01-02 17:15:24 -0500

    Configuring the image uploader for production.
    The image uploader developed in Section 13.4.3:Image resizing is good enough for development, but (as seen in the storage :file line in commit: Configuring the image uploader for image resizing.) it uses the local filesystem for storing the images, which isn’t a good practice in production. Instead, we’ll use a cloud storage service to store images separately from our application.
    
    To configure our application to use cloud storage in production, we’ll use the fog gem, as shown in this commit.
    
    There are many choices for cloud storage, but we’ll use one of the most popular and well-supported, Amazon.com’s Simple Storage Service (S3). Here are the essential steps to getting set up:
    
    1. Sign up for an Amazon Web Services account if you don’t have one already. If you signed up for the Cloud9 IDE in Section 1.2.1: Development environment, you already have an AWS account and can skip this step.
    
    2. Create a user via AWS Identity and Access Management (IAM) and record the access key and secret key.
    
    3. Create an S3 bucket (with a name of your choice) using the AWS Console, and then grant read and write permission to the user created in the previous step.

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,14 +1,21 @@
 class PictureUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
-  process resize_to_limit: [400, 400]
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -37,21 +44,14 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
-
 
 end
