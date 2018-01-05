04cad6d618324d53f2440673fa8d26b439a7e04f
carl-mcgruder <carl.mcgruder@gmail.com>
2018-01-05 00:02:31 -0500

    The following and followers actions. red
    The only tricky part of the implementation is realizing that we need to
    add two new actions to the Users controller. Based on the routes defined
    in commit: Adding following and followers actions to the Users
    controller, we need to call them following and followers. Each action
    needs to set a title, find the user, retrieve either @user.following or
    @user.followers (in paginated form), and then render the page. The
    result appears in this commit.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
-    #redirect_to root_url and return unless @users.where(activated: true)  # Answer to Ex. 2
+    #redirect_to root_url and return unless @user.activated?
+    #redirect_to root_url #and return unless @users.where(activated: true)  # Answer to Ex. 2
   end
 
   def new
@@ -47,6 +48,20 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
+  end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
